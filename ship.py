408e95bbbe701a23c553e6fcfba1926a332f7799
Abumahdiii <mohammad.mes1373@gmail.com>
2024-04-21 00:53:11 +0330

    ship speed added

--- ship.py
+++ ship.py
@@ -7,13 +7,17 @@ class Ship:
 
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
-
+        self.settings = ai_game.settings
+        
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
-
+        
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+        
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
         
         # Movement flag
         self.moving_right = False
@@ -26,6 +30,8 @@ class Ship:
     def update(self):
         """Update the ship's position based on the movement flag."""
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+            
+        self.rect.x = self.x
